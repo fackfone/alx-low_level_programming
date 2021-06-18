b590fe08ff7d128a94214857a275de383fa95657
fackfone <foneevanslindel36@gmail.com>
2021-06-18 15:51:29 -0700

    The new features added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
- * Return - Always 0 (Success)
+ *main - Entry point
+ *
+ *Return: Always 0(Success)
  */
 int main(void)
 {
-	int n;
+	int n, modulo10 = 0;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ()
-	putchar(10);
+	modulo10 = n % 10;
+	if (modulo10 == 0)
+	{
+		printf("Last digit of %d is %d and is 0 \n", n, modulo10);
+	}
+	else if (modulo10 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, modulo10);
+	}
+	else if (modulo10 < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, modulo10);
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,17 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 0, counter = 0;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		if (i != 10)
+		if (counter != 9)
 		{
 			putchar(44);
 			putchar(32);
 		}
+	counter++;
 	}
 	putchar(10);
 	return (0);
