e47412ea141d4eba64e4d8e17ab0fba2154e78fb
fackfone <foneevanslindel36@gmail.com>
2021-06-18 12:04:03 -0700

    Fix the issues with the file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  *main - Entry point
  *
- *Return - Always 0(Success)
+ *Return: Always 0(Success)
  */
 int main(void)
 {
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf(" %d is zero", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf(" %d is negative", n);
 	}
 	else
 	{
-		printf("%d is positive", n);
+		printf(" %d is positive", n);
 	}
 	return (0);
 }
