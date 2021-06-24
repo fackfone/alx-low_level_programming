e7dd8b4ebd4889b7094751f13226bfa6a790eccf
fackfone <foneevanslindel36@gmail.com>
2021-06-24 12:10:01 -0700

    Add the files to the repository

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-void _isupper(int c);
-int largest_number(int a, int b, int c);
+void _putchar(int i);
+int _isupper(int c);
+int _isdigit(int c);
