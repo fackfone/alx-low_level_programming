dd6ad90630665d19c29d10d8ecdee62fb3f7eed0
fackfone <foneevanslindel36@gmail.com>
2021-06-24 11:33:57 -0700

    Add the 0x04 directory in the repository

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,9 +1,4 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 #include <stdio.h>
 
-void positive_or_negative(int i);
 int _isupper(int c);
-int _putchar(int);
-int largest_number(int a, int b, int c);
+void _putchar(int);
