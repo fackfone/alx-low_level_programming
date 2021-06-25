448d6dab9d505749667d32c9546f3f32abe85be1
fackfone <foneevanslindel36@gmail.com>
2021-06-24 22:45:59 -0700

    Update the files

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: parameter of the function
- * Return: 1 on success, and -1 on error
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "holberton.h"
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,9 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 
 #include <stdio.h>
 
-void _putchar(char);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
