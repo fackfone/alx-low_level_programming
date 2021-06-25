77ca7f41d3ed65ea299afb1647b57d0b7b066e83
fackfone <63653551+fackfone@users.noreply.github.com>
2021-06-25 06:48:54 +0100

    Update 0-isupper1.c

--- 0x04-more_functions_nested_loops/0-isupper1.c
+++ 0x04-more_functions_nested_loops/0-isupper1.c
@@ -1,1 +1,21 @@
+#include "holberton.h"
+#include <ctype.h>
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter of the function
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
