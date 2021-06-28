1e5c3ef09a0ce3ac5e30ba78ae63d0ead6c14bf6
fackfone <foneevanslindel36@gmail.com>
2021-06-28 11:08:06 -0700

    Add the folder 0x05-pointers_arrays_strings to the repository

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer and updates value to 98
+ * @n: argument of the function
+ */
+
+void reset_to_98(int *n)
+{
+	int i = 98;
+	*n = i;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## DESCRIPTION OF TASKS
+
+- 0-reset_to_98.c is a function that takes a pointer to an int as parameter and updates the value it points to 98
+
+sh: 1: q: not found
