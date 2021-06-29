77d2723481f200aca9fee559b3de2260454410d1
fackfone <foneevanslindel36@gmail.com>
2021-06-28 17:55:16 -0700

    Add the file 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* _strcpy - function that copies string from pointer to another pointer
+ * @dest: first  argument of the function
+ * @src: second argument of the function
+ * Return: the pointer value to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(src));
+
+}
