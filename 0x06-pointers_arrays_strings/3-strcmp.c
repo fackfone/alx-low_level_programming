56fabcb6c4456bab7edbbe71337fc72de1b41e10
fackfone <foneevanslindel36@gmail.com>
2021-07-01 04:08:54 -0700

    Update the file by removing trailing whitespaces

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-* _strcmp - function that compares two strings
+ * _strcmp - function that compares two strings
  * @s1: first  argument of the function
  * @s2: second argument of the function
- * Return: 0 if s1 is found in s2  
+ * Return: 0 if s1 is found in s2
  */
 
 int _strcmp(char *s1, char *s2)
