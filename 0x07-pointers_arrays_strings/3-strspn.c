dc36bd09cb426423f0a71bd48457d5a4a30fbbbe
fackfone <foneevanslindel36@gmail.com>
2021-07-05 07:16:17 -0700

    Update 3-strspn.c and 4-strpbrk.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 /**
-* _strspn - Gets the length of a prefix substring
+ * _strspn - Gets the length of a prefix substring
  * @s: first  argument of the function and pointer
  * @accept: second argument of the function
  * Return: bytes from s which consitst of bytes from accept
  */
 
-char *_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	return (strspn(s, accept));
 }
