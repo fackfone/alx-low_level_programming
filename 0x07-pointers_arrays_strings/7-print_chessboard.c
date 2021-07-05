2e7c6199ce175a6b2ed558defc14e00d8687f7fc
fackfone <foneevanslindel36@gmail.com>
2021-07-05 11:47:21 -0700

    Update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,25 +5,3 @@
  * print_chessboard - prints the chessboard
  * @a: first argument and pointer
  */
-
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
-	char val;
-	char *array;
-
-	array = &a[0][0];
-
-	for (i = 0 ; i < 8 ; i++)
-	{
-		for (j = 0 ; j < 8 ; j++)
-		{
-			val = *(array + i * 8 + j);
-			putchar(val);
-			if (j == 7)
-			{
-				putchar('\n');
-			}
-		}
-	}
-}
