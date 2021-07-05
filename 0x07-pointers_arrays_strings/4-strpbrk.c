6f28a4678dcf8622ff57d7f5c97ea54c9311d1ea
fackfone <foneevanslindel36@gmail.com>
2021-07-05 13:43:57 -0700

    Update the file 4strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * _strspbrk - a function that searches a string for any of a set of bytes
+ * _strpbrk - a function that searches a string for any of a set of bytes
  * @s: first  argument of the function and pointer
  * @accept: second argument of the function
  * Return: bytes from s which consitst of bytes from accept
