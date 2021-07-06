af2f967d5c6456d35c52c4eae981e6355f602468
fackfone <foneevanslindel36@gmail.com>
2021-07-06 12:06:08 -0700

    Add the file 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- * set_string - a function that locates a substring
+ * set_string - a function that sets the value of apointer to a char 
  * @s: first  argument of the function and pointer
  * @to: second argument of the function and pointer
- * Return: a pointer to the beginning of the located substring
  */
 
 void set_string(char **s, char *to)
