7988fe98831675143c408886ef9b16dfffe42561
fackfone <foneevanslindel36@gmail.com>
2021-07-07 03:22:12 -0700

    Update the contents in the directory

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,16 @@
  * @s: first argument of the function and pointer
  */
 
-char _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	return (puts(s));
+	if (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		putchar(10);
+	}
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,18 @@
 /**
  * _strlen_recursion - a function that retruns the length of a string
  * @s: first argument of the function and pointer
- *
- * Return: length of strig integer
+ * Return: length of string integer
  */
 
-char _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	return (strlen(s));
+	int i = 0, c;
+
+	if (*s != '\0')
+	{
+		c = i + 1;
+		s++;
+		_strlen_recursion(s);
+	}
+	return (c);
 }
