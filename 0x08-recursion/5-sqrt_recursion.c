32a07e4d33d3d71b9ac22f092d0ff16c27244448
fackfone <foneevanslindel36@gmail.com>
2021-07-07 11:23:40 -0700

    Update files prime_number, sqrt recursion and add palindrome

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,24 @@
 #include <string.h>
 #include <stdio.h>
 
+
+
+/**
+ * eval_sqrt - check if square root of n exists
+ * @n: parameter of function
+ * @tmp: parameter iterative to check value
+ * Return: -1 or the square root of n
+ */
+
+int eval_sqrt(int tmp, int n)
+{
+	if (tmp * tmp == n)
+		return (tmp);
+	if (tmp * tmp > n)
+		return (-1);
+	return (eval_sqrt(tmp + 1, n));
+}
+
 /**
  * _sqrt_recursion - estimates square root of n
  * @n: parameter of function
@@ -10,10 +28,7 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n % )
-		return (-1);
-	if (n == 0 || n == 1)
-		return (1);
-	if (n > 0)
-		return (n * factorial(n - 1));
+	if (n == 0)
+		return (0);
+	return (eval_sqrt(1, n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,18 +2,43 @@
 #include <string.h>
 #include <stdio.h>
 
+
+
 /**
- * is_prime_number - evaluates if n is prime number
+ * eval_prime - evaluates if number is prime number
+ * @first_digit: parameter of function
+ * @last_digit: parameter of function
+ * Return: 1 if prime number and 0 otherwise
+ */
+
+int eval_prime(int first_digit, int last_digit)
+{
+	if (last_digit < 2 || last_digit % first_digit == 0)
+	{
+		return (0);
+	}
+	else if (first_digit > last_digit / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (eval_prime(first_digit + 1, last_digit));
+	}
+}
+
+
+/**
+ * is_prime_number - states if n is prime number
  * @n: parameter of function
  * Return: 1 if prime number and 0 otherwise
  */
 
 int is_prime_number(int n)
 {
-	if (n == 1 || n == 0 || n < 0 )
+	if (n == 1 || n == 0 || n < 0)
+	{
 		return (0);
-	if (n % 3 == 0 || n == )
-		return (1);
-	if (n > 0)
-		return (n * factorial(n - 1));
+	}
+	return (eval_prime(2, n));
 }
