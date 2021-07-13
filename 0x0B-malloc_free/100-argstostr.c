8687b17bd1f0fa9911f689a4fd76434cb81c3e9d
fackfone <foneevanslindel36@gmail.com>
2021-07-13 09:30:45 -0700

    Add 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: first paramater (integer)
+ * @av: second paramater (character)
+ * Return: Arguments followed by a new line, NULL otherwise
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *ptr;
+
+	ptr = malloc(ac);
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ac >= 1)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j] != 0; j++, counter++)
+			{
+				ptr[counter] = av[i][j];
+			}
+			ptr[counter] = '\n';
+			counter++;
+		}
+		ptr[counter] = '\0';
+	}
+	return (ptr);
+}
