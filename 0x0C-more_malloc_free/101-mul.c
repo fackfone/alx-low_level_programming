b078b8facc264210b722c15bef3654d2535f42ec
fackfone <foneevanslindel36@gmail.com>
2021-07-15 17:23:40 -0700

    Add 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,40 @@
 #include "holberton.h"
 
 /**
+ * _atoi - Converts chars in strings
+ * @str: first argument char
+ * Return: The string converted into string
+ */
+int _atoi(char *str)
+{
+	int res = 0, i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		res = res * 10 + str[i] - '0';
+	}
+	return (res);
+}
+
+/**
+ * print - print integers to the screen
+ * @integer: first parameter integer
+ * Return: Nothing
+ */
+void print(int integer)
+{
+	int remainder;
+
+	if (integer > 9)
+	{
+		remainder = integer / 10;
+		integer -= 10 * remainder;
+		print(remainder);
+	}
+	_putchar('0' + integer);
+}
+
+/**
  * main - Entry point
  * @argc: first paramater (argument count)
  * @argv: second paramater (argument vector)
@@ -17,12 +51,18 @@ int main(int argc, char *argv[])
 	{
 		int1 = _atoi(argv[1]);
 		int2 = _atoi(argv[2]);
-		printf("%d\n", int1 * int2);
+		print(int1 * int2);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit(98);
 	}
+	_putchar('\n');
 	return (0);
 }
