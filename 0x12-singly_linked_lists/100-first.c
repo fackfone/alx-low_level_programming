56b0edde2ce95e61fdd73b99fe70fbc11fcf6cd2
fackfone <foneevanslindel36@gmail.com>
2021-07-30 02:40:15 +0100

    Add singly linked lists, contents and README

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * printText - Function that prints a paragraph at compiling
+ *
+ * Return:Nothing
+ */
+
+void printText(void)
+{
+	printf(
+		"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+			);
+}
