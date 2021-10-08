2c010d07196959fce8fa1c995177a4f5e292bbb7
fackfone <foneevanslindel36@gmail.com>
2021-10-08 21:40:15 +0100

    Add the files and the folder

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,4 +1,10 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 #include <stddef.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -28,3 +34,4 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 }hash_table_t;
+#endif
